9cddef3e866fda191fec19459cab402415bb0cff
Alex Moser <73650025+Grado0815@users.noreply.github.com>
2021-01-22 18:08:03 +0100

    asd

---
+++ Roll-A-Ball-GAME-main/Assets/Scripts/GameManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+
+
+    public void EnterLevel()
+    {
+        SceneManager.LoadScene("Roll-A-Ball");
+    }
+
+    public void ReloadLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+
+    }
+
+    public void ExitGame()
+    {
+        //yield return new WaitForSeconds(5);
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.ExitPlaymode();
+#endif
+        // works only with built Apps
+        // Application.Quit();
+
+    }
+}
