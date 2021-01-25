e181c723905e05d3ce4f8d803ea2eb64fb09d5c7
Alex Moser <73650025+Grado0815@users.noreply.github.com>
2021-01-25 15:05:57 +0100

    Fixed GameManager / PlayerController

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,6 +5,13 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    public GameObject completeLevelUI;
+
+    public void CompleteLevel()
+    {
+        completeLevelUI.SetActive(true);
+    }
+    
     public void EndGame()
     {
         Debug.Log(message:"Game Over");
