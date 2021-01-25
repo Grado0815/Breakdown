3ad1d42e648647155639e8d5abd8135b41af915b
Alex Moser <73650025+Grado0815@users.noreply.github.com>
2021-01-25 13:27:55 +0100

    GameManager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public void EndGame()
+    {
+        Debug.Log(message:"Game Over");
+    }
+    
+    
+
+   /* public void EnterLevel()
+    {
+        SceneManager.LoadScene("Roll-A-Ball");
+    }
+
+    public void ReloadLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+
+    }
+
+    public void ExitGame()
+    {
+        //yield return new WaitForSeconds(5);
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.ExitPlaymode();
+#endif
+        // works only with built Apps
+        // Application.Quit();
+
+    }*/
+}
